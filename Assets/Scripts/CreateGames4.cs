3a5e8dc1e54637b06c44af70b563aff707717967
Naufal <naufalmahfudzismail@gmail.com>
2018-02-22 05:53:43 +0700

    Game 3 match color

---
+++ Assets/Scripts/CreateGames4.cs
@@ -0,0 +1,393 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tile
+{
+	public GameObject tileObject;
+	public string Type;
+
+	public Tile(GameObject tileObject, string Type)
+	{
+		this.tileObject = tileObject;
+		this.Type = Type;
+	}
+}
+
+public class CreateGames4 : MonoBehaviour {
+
+	GameObject tile1 = null;
+	GameObject tile2 = null;
+
+	GameObject tile3 = null;
+	GameObject tile4 = null;
+	GameObject tile5 = null;
+
+	Ball ball1;
+	Ball ball2;
+	Ball ball3;
+
+	public int Target;
+	private int TValue = 0;
+
+	public GameObject[] tile;
+
+	List<GameObject>tileBank = new List<GameObject>();
+	List<GameObject> select = new List<GameObject> (new GameObject[3]);
+
+	static int baris = 9;
+	static int kolom = 6;
+
+	Tile[,] tiles = new Tile[kolom, baris];
+
+	//private GameObject[] selected = new GameObject[3];
+	private bool renewBoard = false;
+
+	// Use this for initialization
+
+	private void Shuffle()
+	//random cloning object
+	{
+		System.Random rand = new System.Random();
+		int r = tileBank.Count;
+		while (r > 1) {
+
+			r--;
+			int n = rand.Next (r + 1);
+			GameObject val = tileBank [n];
+			tileBank [n] = tileBank [r];
+			tileBank [r] = val;
+
+
+		}
+
+	}
+	void Start () {
+
+
+
+		// clone object in border
+		int jumlah = (baris * kolom) / 3;
+
+		for (int i = 0; i < jumlah; i++) {
+
+			for (int j = 0; j < tile.Length; j++) {
+
+				GameObject obj = (GameObject)Instantiate (tile [j], new Vector3 (-10, -10, 0), tile[j].transform.rotation);
+				obj.SetActive (false);
+				tileBank.Add (obj);
+				
+			}
+
+			Shuffle ();
+				
+		}
+
+		//array 2x2 matrix 
+
+		for (int b = 0; b < baris; b++) {
+
+			for (int k = 0; k < kolom; k++) {
+				Vector3 tilePos = new Vector3 (k, b, 0);
+
+				for (int n = 0; n < tileBank.Count; n++) {
+
+					GameObject obj = tileBank [n];
+
+					if (!obj.activeSelf) {
+
+						obj.transform.position = new Vector3 (tilePos.x, tilePos.y, tilePos.z);
+						obj.SetActive (true);
+						tiles [k, b] = new Tile (obj, obj.name);
+						n = tileBank.Count + 1;
+					}
+						
+				}
+			}
+		}
+
+		Debug.Log ("Naufal");			
+		
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if (ball1 != null && ball2 != null || ball3 != null) {
+			ball1 = selected [0].GetComponent<Ball> ();
+			ball2 = selected [1].GetComponent<Ball> ();
+			ball3 = selected [2].GetComponent<Ball> ();
+
+			TValue = ball1.getValue () + ball2.getValue () + ball3.getValue ();
+		}*/
+
+
+		CheckGrid ();
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, 1000);
+				
+			if (hit) {
+				Debug.Log("hit");
+				tile1 = hit.collider.gameObject;
+			}
+		} 
+
+		else if (Input.GetMouseButtonUp (0) && tile1){
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, 1000);
+
+			if (hit) {
+				tile2 = hit.collider.gameObject;
+			}
+
+			if (tile1 && tile2) {
+
+			
+
+				int horzDist = (int)Mathf.Abs (tile1.transform.position.x - tile2.transform.position.x);
+				int vertDist = (int)Mathf.Abs (tile1.transform.position.y - tile2.transform.position.y);
+
+				// condition limit swap vertical or horizontal 1 block
+				if (horzDist == 1 ^ vertDist == 1) {
+
+					//swap object position and turn back if condition is false while di seret
+					Tile temp = tiles [(int)tile1.transform.position.x, (int)tile1.transform.position.y];
+					tiles [(int)tile1.transform.position.x, (int)tile1.transform.position.y] = tiles [(int)tile2.transform.position.x, (int)tile2.transform.position.y];
+					tiles [(int)tile2.transform.position.x, (int)tile2.transform.position.y] = temp;
+
+					Vector3 tempPos = tile1.transform.position;
+					tile1.transform.position = tile2.transform.position;
+					tile2.transform.position = tempPos;
+
+					//reset touched ball
+
+					tile1 = null;
+					tile2 = null;
+				} 
+
+				else
+				{
+					
+				}
+
+			}
+			
+		}
+
+
+			
+	}
+
+	private void CheckGrid()
+	{
+		int counter = 1;
+
+		//check in columns
+		for (int b = 0; b < baris; b++) {
+
+			counter = 1;
+
+			for (int k = 1; k < kolom; k++) {
+
+				if ( tiles[k, b] != null && tiles[k-1, b] != null){
+					//if tiles exist
+
+					if (tiles [k, b].Type == tiles [k - 1, b].Type) {
+
+						counter++;
+					} 
+
+					else {
+						//reset counter
+						counter = 1;
+					}
+					// if there are found, remove them
+					if (counter == 3) {
+						if (tiles [k, b] != null)
+							ball1 = tiles [k, b].tileObject.GetComponent<Ball> ();
+						if (tiles [k - 1, b] != null)
+							ball2 = tiles [k - 1, b].tileObject.GetComponent<Ball> ();
+						if (tiles [k - 2, b] != null)
+							ball3 = tiles [k - 2, b].tileObject.GetComponent<Ball> ();
+						TValue = ball1.getValue () + ball2.getValue () + ball3.getValue ();
+						//selectObj ();
+						if (Target <= TValue) {
+
+							if (tiles [k, b] != null)
+								tiles [k, b].tileObject.SetActive (false);
+							if (tiles [k - 1, b] != null)
+								tiles [k - 1, b].tileObject.SetActive (false);
+							if (tiles [k - 2, b] != null)
+								tiles [k - 2, b].tileObject.SetActive (false);
+							tiles [k, b] = null;
+							tiles [k - 1, b] = null;
+							tiles [k - 2, b] = null;
+							renewBoard = true;
+						}
+					}
+				}
+			}
+		}
+
+		//check in rows
+		for (int k = 0; k < kolom; k++) {
+
+			counter = 1;
+
+			for (int b = 1; b < baris; b++) {
+				
+				if ( tiles[k, b] != null && tiles[k, b-1] != null){
+					//if tiles exist
+
+					if (tiles [k, b].Type == tiles [k , b-1].Type) {
+
+						counter++;
+					} 
+
+					else {
+						counter = 1;
+					}
+
+					if (counter == 3) {
+						
+						if (tiles [k, b] != null)
+							ball1 = tiles [k, b].tileObject.GetComponent<Ball> ();
+						if (tiles [k, b - 1] != null)
+							ball2 = tiles [k, b - 1].tileObject.GetComponent<Ball> ();
+						if (tiles [k, b - 2] != null)
+							ball3 = tiles [k, b - 1].tileObject.GetComponent<Ball> ();
+						
+						TValue = ball1.getValue () + ball2.getValue () + ball3.getValue ();
+
+						//selectObj ();
+
+						if (Target <= TValue) {
+
+							if (tiles [k, b] != null)
+								tiles [k, b].tileObject.SetActive (false);
+							if (tiles [k, b - 1] != null)
+								tiles [k, b - 1].tileObject.SetActive (false);
+							if (tiles [k, b - 2] != null)
+								tiles [k, b - 2].tileObject.SetActive (false);
+							tiles [k, b] = null;
+							tiles [k, b - 1] = null;
+							tiles [k, b - 2] = null;
+							renewBoard = true;
+						}
+					}
+				}
+			}
+		}
+
+		if (renewBoard) {
+
+			newGrid ();
+			renewBoard = false;
+		}
+	}
+
+	private void newGrid()
+	{
+		bool moved = false;
+		Shuffle ();
+
+
+		for (int b = 1; b < baris; b++) {
+
+			for (int k = 0; k < kolom; k++) {
+
+				if (b == baris - 1 && tiles [k, b] == null) {
+
+					Vector3 tilePos = new Vector3 (k, b, 0);
+
+					for (int i = 0; i < tileBank.Count; i++) {
+
+						GameObject obj = tileBank [i];
+
+						if (!obj.activeSelf) {
+							
+							obj.transform.position = new Vector3 (tilePos.x, tilePos.y, tilePos.z);
+							obj.SetActive (true);
+							tiles [k, b] = new Tile (obj, obj.name);
+							i = tileBank.Count + 1;
+						}
+					}
+				}
+
+				if (tiles [k, b] != null) {
+
+					//dropDown if below is empty
+
+					if (tiles [k, b - 1] == null) {
+
+						tiles [k, b - 1] = tiles [k, b];
+						tiles [k, b - 1].tileObject.transform.position = new Vector3 (k, b - 1, 0);
+						tiles [k, b] = null;
+						moved = true;
+					}
+				}
+			}
+
+			if (moved) {
+
+				Invoke ("newGrid", 0.5f);
+			}
+		}
+	}
+
+	/*private void selectObj()
+	{
+		int counters = 0;
+		if (Input.GetMouseButtonDown (1)) {
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D hit = Physics2D.GetRayIntersection (ray, 1000);
+
+			if (hit) {
+				
+				Debug.Log("Selected");
+
+
+				if (counters >= 0) {
+					counters = counters + 1;
+
+				}
+			}
+
+			if (counters == 1) {
+				tile3 = hit.collider.gameObject;
+				select.Add (tile3);
+			}
+
+			if (counters == 2) {
+				tile4 = hit.collider.gameObject;
+				select.Add (tile4);
+			}
+
+			if (counters == 3) {
+				tile5 = hit.collider.gameObject;
+				select.Add (tile5);
+			}
+
+
+		}
+
+		if (tile3 != null && tile4 != null && tile5 != null) {
+
+			ball1 = select [0].GetComponent<Ball> ();
+			ball2 = select [1].GetComponent<Ball> ();
+			ball3 = select [2].GetComponent<Ball> ();
+
+			TValue = ball1.getValue () + ball2.getValue () + ball3.getValue ();
+
+		}
+			
+	}*/
+			
+
+
+	}
+
